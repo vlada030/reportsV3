38fa2818b2d1008815c71c6e333adb8b7ea6f73f
Vlada FKZ Laptop <vlada030@gmail.com>
2022-03-23 14:50:24 +0100

    zapoceta integratcija react-routera v6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
-import Bezveze from "./Bezveze";
-import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom'
+import Bezveze from "./Bezveze"
+import {BrowserRouter as Router, Routes,  Link, Route} from 'react-router-dom'
 import {Navbar, Sidebar, Modal} from './components'
+import { LandingPage} from './pages'
 
 const App = () => {
     return (
         <Router>
-            <Navbar/>
-            <Sidebar/>
-            <Modal/>
+            <Navbar />
+            <Sidebar />
+            <Modal />
+           
+            <Routes>
+                <Route path="/test" element={<Bezveze />} />                
+            </Routes>
+            
         </Router>
-    )
+    );
 }
 
 export default App;
