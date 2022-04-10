745dff9d69a2b9dce6a0df59a74ba36b3fc501a2
Vlada DELL <vlada030@gmail.com>
2022-04-10 21:53:46 +0200

    authentication page wrapper

--- frontend/src/pages/AuthenticationPage.js
+++ frontend/src/pages/AuthenticationPage.js
@@ -1,7 +1,7 @@
 function AuthenticationPage() {
     return (
-        <section className="bg-fkz-500 h-screen ">
-            LOGIN REGISTER PAGE
+        <section className="min-h-screen bg-login-hero bg-no-repeat bg-cover bg-center relative after:content-[''] after:absolute after:w-full after:h-full after:bg-fkz-300/60 flex items-center justify-center" >
+            <h2 className="bg-white w-32 h-32 z-10">PROOOOBAAA</h2>
         </section>
     );
 }
